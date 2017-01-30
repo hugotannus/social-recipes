58a5ab03889111ad782b7f3edf4a893edbbb7488
Hugo Tannús <hctannus@gmail.com>
2017-01-30 05:36:31 -0200

    Specs defined for new feature.

---
+++ spec/features/user_views_recipes_by_cuisine_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+feature 'User views recipes by cuisine' do
+  scenario 'successfully' do
+    cuisine = create(:cuisine)
+    recipe_a = create(:recipe, cuisine: cuisine)
+    recipe_b = create(:recipe, cuisine: cuisine,
+                               kind: recipe_a.kind)
+    visit root_path
+
+    click_on cuisine.name
+
+    expect(page).to have_css('h1', text: cuisine.name.downcase)
+    expect(page).to have_content(recipe_a.title)
+    expect(page).to have_content(recipe_b.title)
+  end
+
+  scenario 'and view only the recipes from current cuisine' do
+    cuisine_1 = create(:cuisine)
+    cuisine_2 = create(:cuisine, name: 'principal')
+
+    recipe_a = create(:recipe, cuisine: cuisine_1)
+    recipe_b = create(:recipe, cuisine: cuisine_2,
+                               kind: recipe_a.kind)
+
+    visit root_path
+    click_on cuisine_2.name
+
+    expect(page).to have_css('h1', text: cuisine_2.name)
+    expect(page).to have_content recipe_b.title
+    expect(page).not_to have_content recipe_a.title
+    expect(page).not_to have_content cuisine_1.name
+  end
+
+  scenario 'view a friendly empty message' do
+    cuisine = Cuisine.create(name: 'sobremesas')
+
+    visit root_path
+    click_on cuisine.name
+
+    expect(page).to have_content 'Nenhuma receita adicionada.'
+  end
+
+end
